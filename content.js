d8d47958622846af0d3dc46df514bfdba85cede5
Kevin Bigelow <kevinbigelow@Kevins-iMac.local>
2018-02-11 12:52:59 -0700

    loop through people and company names for each target element. add external stylesheet

--- content.js
+++ content.js
@@ -1,6 +1,99 @@
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
+var companyNames = {
+    1: 'Acme Corporation',
+    2: 'Globex Corporation',
+    3: 'Soylent Corp',
+    4: 'Initech',
+    5: 'Bluth Company',
+    6: 'Umbrella Corporation',
+    7: 'Hooli',
+    8: 'Vehement Capital Partners',
+    9: 'Massive Dynamic',
+    10: 'Wonka Industries',
+    11: 'Stark Industries',
+    12: 'Gekko & Co',
+    13: 'Wayne Enterprises',
+    14: 'Bubba Gump',
+    15: 'Cyberdyne Systems',
+    16: 'Genco Pura Olive Oil Company',
+    17: 'The New York Inquirer',
+    18: 'Duff Beer',
+    19: 'Olivia Pope & Associates',
+    20: 'Sterling Cooper',
+    21: 'Ollivanders Wand Shop',
+    22: 'Cheers',
+    23: 'Krusty Krab',
+    24: 'Good Burger',
+}
+
+var peopleNames = {
+    1: 'Bradley Mueller',
+    2: 'Clara Heath',
+    3: 'Davis Logan',
+    4: 'Yosef Chase',
+    5: 'Cristofer Dunlap',
+    6: 'Madelynn Foley',
+    7: 'Rodolfo Scott',
+    8: 'Adelyn Fletcher',
+    9: 'Donovan Duffy',
+    10: 'Jarrett Kelley',
+    11: 'Nelson Brock',
+    12: 'Jaime Knight',
+    13: 'Moses Wolfe',
+    14: 'Alan Morgan',
+    15: 'Ronin Holder',
+    16: 'Kamora Rios',
+    17: 'Jaidyn Walls',
+    18: 'Madelyn Collier',
+    19: 'Grayson Stanton',
+    20: 'Kennedy Green',
+    21: 'Josephine Rojas',
+    22: 'Alexis Hicks',
+    23: 'Andrea Church',
+    24: 'Gloria Cameron',
+    25: 'Noelle Carson',
+    26: 'Jamar Griffith',
+    27: 'Marshall Ray',
+    28: 'Xander Conrad',
+    29: 'Dante Burke',
+    30: 'Tristian Hartman',
+}
+
 function gotMessage(message, sender, sendResponse) {
-    $(message.target).text(message.content);
+
+    var target = message.target,
+        targetCount = $('body ' + target).length,
+        content = message.content,
+        namesCount = Object.keys(peopleNames).length,
+        companiesCount = Object.keys(companyNames).length;
+
+    if (content == 'names') {
+        var i = 1;
+        
+        $(target).each(function(){
+            $(this).text(peopleNames[i]);
+            if (i === namesCount) {
+                i = 1
+            } else {
+                i = i + 1
+            }
+        });
+
+    } else if (content == 'companies') {
+        var i = 1;
+
+        $(target).each(function(){
+            $(this).text(companyNames[i]);
+            if (i === companiesCount) {
+                i = 1
+            } else {
+                i = i + 1
+            }
+        });
+
+    } else {
+        $(message.target).text(message.content);
+    }
 }
